8844b06d3eb22f2e250cd7c89e8c9e26c5820c13
Masaya Kaneko <syinari0123@gmail.com>
2020-08-11 19:31:30 +0900

    Made the mount directory changeable

--- scripts/docker/run_docker.sh
+++ scripts/docker/run_docker.sh
@@ -1,8 +1,11 @@
+# You can change this mount directory to your environment.
+DATA_PATH="/home/$(id --user --name)"
+
 xhost +local:
 docker run -it \
     --rm \
     --runtime=nvidia \
-    -v /home/$(id --user --name):/home/$(id --user --name) \
+    -v ${DATA_PATH}:${DATA_PATH} \
     -e DISPLAY=$DISPLAY \
     -v /tmp/.X11-unix/:/tmp/.X11-unix:ro \
     deepfactors
