eaf4e27e23c789e268a56bf7812bf0611a59acee
Masaya Kaneko <syinari0123@gmail.com>
2020-08-11 19:20:07 +0900

    Added a command to check if the weights have aready been downloaded in advance

--- scripts/download_network.bash
+++ scripts/download_network.bash
@@ -1,4 +1,11 @@
 #!/bin/bash
-mkdir -p data/nets
 URL="https://www.dropbox.com/s/fj5dfq9hkqv2hm1/scannet256_32.tar.gz"
-wget -qO - ${URL} | tar xvz -C data/nets 
+WEIGHT_PATH="data/nets"
+
+if [[ ! -d ${WEIGHT_PATH} ]]; then
+  # Download pretrained weights
+  mkdir -p ${WEIGHT_PATH}
+  wget -qO - ${URL} | tar xvz -C ${WEIGHT_PATH}
+else
+  echo "Pretrained weights already exists in ${WEIGHT_PATH}!"
+fi
